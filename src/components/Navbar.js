f72cf7bc5b6bc4cd9cd83f46b3d0526515280c3e
oscaramontiel <etherealflux94@gmail.com>
2023-12-22 15:56:46 -0600

    Fix display bug in title Header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -128,12 +128,12 @@ const Navbar = () => {
         <Link to="/">
           <h1 className={navbarStyles.title}>
             <AniLink fade to="/">
-              <span style={{ fontWeight: 'normal' }}>
-                {' '}
-                {tesisAntitesis.tesis} — {tesisAntitesis.antitesis}{' '}
-              </span>
               <span className={navbarStyles.antitesis} style={{ fontWeight: 'normal' }}>
-                | <b>metaxis.digital</b>
+                {' '}
+                {tesisAntitesis.tesis} — {tesisAntitesis.antitesis} |{' '}
+              </span>
+              <span>
+                <b>metaxis.digital</b>
               </span>{' '}
             </AniLink>{' '}
           </h1>
