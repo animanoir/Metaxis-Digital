f26b3cc5365b3e62050c75128bea34c2f6a58e70
oscaramontiel <etherealflux94@gmail.com>
2024-01-25 00:15:39 -0600

    Update Biblioteca.js and jy-meditation.mdx

--- src/pages/Biblioteca.js
+++ src/pages/Biblioteca.js
@@ -53,7 +53,7 @@ const Biblioteca = ({ data }) => {
         <div className={bibliotecaStyles.contentContainer}>
           <h1 className={bibliotecaStyles.title}>Biblioteca</h1>
           <p className={bibliotecaStyles.text}>
-            La gran biblioteca de <b>metaxis.digital</b> (en constante actualización)
+            La <b>Gran Biblioteca</b> de <b>metaxis.digital</b> (en constante actualización)
             <span className={bibliotecaStyles.typingDots}></span>
           </p>
           {/* Book List */}
