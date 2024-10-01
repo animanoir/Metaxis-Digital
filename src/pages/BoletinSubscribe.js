3308e6cf992cdd011a482a328680d9bc208d406e
oscaramontiel <etherealflux94@gmail.com>
2024-09-30 19:08:03 -0600

    refactor: Update BoletinSubscribe page styles

--- src/pages/BoletinSubscribe.js
+++ src/pages/BoletinSubscribe.js
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import Layout from '../components/Layout';
-import { graphql } from 'gatsby';
+import * as acercaStyles from '../css/Acerca.module.css';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+
 
 // export const query = graphql`
 //   query {
@@ -28,11 +30,17 @@ import { graphql } from 'gatsby';
 const BoletinSubscribe = ({ data }) => {
   return (
     <Layout>
-      <h1
-        style={{ fontSize: '2rem', textAlign: 'center', fontFamily: 'Karla', marginTop: '10rem' }}
-      >
-        ¡Estás suscrito! Ahora checa tu cuenta bancaria 😉.
-      </h1>
+      <div className={acercaStyles.container}>
+      <div className={acercaStyles.contentContainer}>
+        <h1 className={acercaStyles.title}>¡Felicidades! ¡Estás suscrito! Ahora checa tu cuenta bancaria 😉.</h1>
+        <p className={acercaStyles.text}>
+          Nah, nocierto—no somos de los que acceden a tu cuenta con tu puro email (pero otros sí). Ahora recibirás, de vez en cuando, nuestro último contenido y eventos.
+        </p>
+        <p className={acercaStyles.text}>
+          Recuerda compartir el siguiente enlace a las personas que más amas en tu vida para que también se suscriban: <AniLink style={{textDecoration: "underline"}} fade to="/SuscribirseBoletin"><b>¡Quiero compartir metaxis.digital con la gente que amo!</b></AniLink>
+        </p>
+      </div>
+    </div>
     </Layout>
   );
 };
