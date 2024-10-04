467c315b99e272e9cbaede1d5b717f7152f7482d
oscaramontiel <etherealflux94@gmail.com>
2024-10-03 22:19:22 -0600

    refactor: Update package.json and Post.js
    - Update package.json to add a new "format" script for prettier formatting
    - Update Post.js to remove unnecessary imports and optimize code structure
    - Update Posts.module.css to add padding and color to featuredContainer elements

--- src/components/Posts/Post.js
+++ src/components/Posts/Post.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import * as postsStyles from '../../css/Posts.module.css';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
-import { Link } from 'gatsby';
-import AniLink from 'gatsby-plugin-transition-link/AniLink';
-import BookCard from '../BookCard.js';
+import React from 'react'
+import * as postsStyles from '../../css/Posts.module.css'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
+import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import BookCard from '../BookCard.js'
 
 const Post = React.memo(({ frontmatter }) => {
-  const { title, concepts, slug, excerpt, date, author, image, featuredArticle, type } =
-    frontmatter;
+  const { title, concepts, slug, excerpt, date, author, image, featuredArticle, type } = frontmatter
 
   const ConceptLink = ({ concept }) => (
     <Link to={`/concepts/${concept}`} key={concept}>
       <span className={postsStyles.singleconcept}>{concept} </span>
     </Link>
-  );
+  )
 
   const renderPostContent = () => {
     /**
@@ -21,7 +20,7 @@ const Post = React.memo(({ frontmatter }) => {
      * @type {string}
      * @description If the post has the type "libro", the layout of the post will be changed to the BookCard layout.
      */
-    const postType = type;
+    const postType = type
 
     /**
      * The CSS class name for the container element of the post.
@@ -30,9 +29,9 @@ const Post = React.memo(({ frontmatter }) => {
      */
     const containerClass = featuredArticle
       ? postsStyles.featuredContainer
-      : postsStyles.simpleContainer;
+      : postsStyles.simpleContainer
 
-    const imageClass = featuredArticle ? postsStyles.featuredImage : postsStyles.simpleImage;
+    const imageClass = featuredArticle ? postsStyles.featuredImage : postsStyles.simpleImage
 
     return (
       <div className={containerClass}>
@@ -40,37 +39,37 @@ const Post = React.memo(({ frontmatter }) => {
           <BookCard frontmatter={frontmatter} />
         ) : (
           <React.Fragment>
-            <GatsbyImage
-              objectFit="cover"
-              alt={title}
-              className={imageClass}
-              image={getImage(image)}
-            />
             <div>
-              <p className={postsStyles.date}>{date}</p>
               <AniLink fade to={`/posts/${slug}`}>
+                <GatsbyImage
+                  objectFit="cover"
+                  alt={title}
+                  className={imageClass}
+                  image={getImage(image)}
+                />
+                <p className={postsStyles.date}>{date}</p>
                 <h2>{title}</h2>
+                <h4>{excerpt}</h4>
+                <h5>
+                  <span style={{ fontWeight: 'normal' }}>por </span>
+                  <AniLink fade to={`/autores/${author}`}>
+                    {author}
+                  </AniLink>
+                </h5>
+                <h6 className={postsStyles.conceptsContainer}>
+                  {concepts.map((concept) => (
+                    <ConceptLink key={concept} concept={concept} />
+                  ))}
+                </h6>
               </AniLink>
-              <h4>{excerpt}</h4>
-              <h5>
-                <span style={{ fontWeight: 'normal' }}>por </span>
-                <AniLink fade to={`/autores/${author}`}>
-                  {author}
-                </AniLink>
-              </h5>
-              <h6 className={postsStyles.conceptsContainer}>
-                {concepts.map((concept) => (
-                  <ConceptLink key={concept} concept={concept} />
-                ))}
-              </h6>
             </div>
           </React.Fragment>
         )}
       </div>
-    );
-  };
+    )
+  }
 
-  return <div>{renderPostContent()}</div>;
-});
+  return <div>{renderPostContent()}</div>
+})
 
-export default Post;
+export default Post
